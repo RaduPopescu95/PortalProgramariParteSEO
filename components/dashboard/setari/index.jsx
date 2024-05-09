31d1953adefc1595dc05d37676086b7c204bb33b
Adrian <radu.p1995@yahoo.com>
2024-05-09 21:14:53 +0300

    09.05.2024

--- components/dashboard/setari/index.jsx
+++ components/dashboard/setari/index.jsx
@@ -86,7 +86,7 @@ const index = () => {
                   </div>
                   <div className="my_dashboard_review mt30">
                     <div className="col-lg-12">
-                      <h3 className="mb30">Setari Google</h3>
+                      <h3 className="mb30">Setari</h3>
                     </div>
                     <PropertyMediaUploader />
                   </div>
