15dc5837f73fe949f9ee7a5088450a1dfe3e2daf
Adrian <radu.p1995@yahoo.com>
2024-06-26 15:52:16 +0300

    asda

--- app/robots.js
+++ app/robots.js
@@ -2,7 +2,7 @@ export default function robots() {
   return {
     rules: {
       userAgent: "*",
-      allow: "/",
+      Disallow: "/",
     },
     sitemap: `${process.env.NEXT_PUBLIC_SITE_URL}/sitemap.xml`,
   };
